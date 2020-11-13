c7d2b637e6409f2eae33589e06f74b1424f6efc9
agustin.galimberti <agustin.galimberti@tsoftlatam.com>
2020-11-13 10:36:30 -0300

    reemplazo de arquetipo, modificacion por error de compilacion

---
+++ src/test/java/seleniumCode/Test.java
@@ -0,0 +1,32 @@
+package seleniumCode;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Test {
+    @Given(": como cliente ingreso a la URL {string}")
+    public void como_cliente_ingreso_a_la_url(String string) throws Exception {
+
+    }
+
+    @When(": selecciono un destino.")
+    public void selecciono_un_destino() throws Exception {
+
+    }
+
+    @When(": Lo agrego a la bolsa.")
+    public void lo_agrego_a_la_bolsa() throws Exception {
+
+    }
+
+    @When(": escojo un medio de pago.")
+    public void escojo_un_medio_de_pago() throws Exception {
+
+    }
+
+    @Then(": y simulo la compra.")
+    public void y_simulo_la_compra() throws Exception {
+
+    }
+}
